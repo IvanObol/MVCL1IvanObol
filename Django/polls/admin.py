a8a91ab038c08b08081083bb3bbea3243bb54398
IvanObol <oboliv6343_aehit@students.vizja.pl>
2026-03-06 18:42:15 +0100

    4/7 stron

---
+++ Django/polls/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3  # Сразу предложит 3 пустых поля для ответов
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
