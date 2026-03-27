ddc4b1a37c5584e4d4fc668269fef069898aa13b
IvanObol <oboliv6343_aehit@students.vizja.pl>
2026-03-27 20:34:25 +0100

    lab 5 testy

--- Django/polls/tests.py
+++ Django/polls/tests.py
@@ -1,51 +1,81 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
-from django.urls import reverse  # Перенесли сюда
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def create_question(question_text, days):
-    """Shortcut to create a question with a specific date offset."""
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
-        """was_published_recently() returns False for questions in the future."""
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
-        """was_published_recently() returns False for questions older than 1 day."""
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
-        """was_published_recently() returns True for questions within the last day."""
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
-        """If no questions exist, an appropriate message is displayed."""
         response = self.client.get(reverse("polls:index"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context["latest_question_list"], [])
 
     def test_past_question(self):
-        """Past questions are displayed on the index page."""
         question = create_question(question_text="Past question.", days=-30)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerySetEqual(response.context["latest_question_list"], [question])
 
     def test_future_question(self):
-        """Future questions aren't displayed."""
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse("polls:index"))
         self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_question_list"], [question])
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
+class QuestionResultsViewTests(TestCase):
+    def test_results_for_past_question(self):
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:results", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+    def test_voting_increments_count(self):
+        question = create_question(question_text="Vote test.", days=-1)
+        choice = Choice.objects.create(question=question, choice_text="Option 1", votes=0)
+        
+        self.client.post(reverse("polls:vote", args=(question.id,)), {'choice': choice.id})
+        
+        choice.refresh_from_db()
+        self.assertEqual(choice.votes, 1)
