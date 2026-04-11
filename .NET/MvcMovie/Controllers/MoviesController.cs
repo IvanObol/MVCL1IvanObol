8666b76d3e16d54eadb469efd03065171f5550a4
IvanObol <oboliv6343_aehit@students.vizja.pl>
2026-04-11 21:51:25 +0200

    lab 6 .NET chyba zrobiąny

--- .NET/MvcMovie/Controllers/MoviesController.cs
+++ .NET/MvcMovie/Controllers/MoviesController.cs
@@ -14,11 +14,28 @@ public class MoviesController : Controller
         _context = context;
     }
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(string searchString)
     {
-        return View(await _context.Movie.ToListAsync());
+        var movies = from m in _context.Movie
+                     select m;
+
+        if (!string.IsNullOrEmpty(searchString))
+        {
+            movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+        }
+
+        return View(await movies.ToListAsync());
     }
 
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+        if (movie == null) return NotFound();
+
+        return View(movie);
+    }
 
     public IActionResult Create()
     {
@@ -27,7 +44,7 @@ public class MoviesController : Controller
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+    public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
     {
         if (ModelState.IsValid)
         {
@@ -36,5 +53,64 @@ public class MoviesController : Controller
             return RedirectToAction(nameof(Index));
         }
         return View(movie);
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var movie = await _context.Movie.FindAsync(id);
+        if (movie == null) return NotFound();
+        
+        return View(movie);
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+    {
+        if (id != movie.Id) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(movie);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Movie.Any(e => e.Id == movie.Id)) return NotFound();
+                else throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(movie);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+        if (movie == null) return NotFound();
+
+        return View(movie);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var movie = await _context.Movie.FindAsync(id);
+        if (movie != null)
+        {
+            _context.Movie.Remove(movie);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
     }
 }
--- .NET/MvcMovie/Program.cs
+++ .NET/MvcMovie/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
+using MvcMovie.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,5 +25,11 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 app.Run();
